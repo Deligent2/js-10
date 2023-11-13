6d87ffd08fc4e89992f420675a539b56ae55f525
Deligent2 <148380705+Deligent2@users.noreply.github.com>
2023-11-13 17:31:43 +0300

    finished

--- script.js
+++ script.js
@@ -13,11 +13,13 @@ const capital = document.querySelector('#capital')
 const region = document.querySelector('#region')
 const text = document.querySelector('#text')
 const maps = document.querySelector('#maps')
+const text1=document.querySelector('#text1')
+
 console.log(all.checked)
 console.log(search.checked)
 
 const handleGetCountries = () => {
-    fetch(`https://restcountries.com/v3.1/all`)
+     fetch('https://restcountries.com/v3.1/all')
         .then(res => res.json())
         .then(data => {
             data.forEach(country => {
@@ -59,7 +61,7 @@ submit.addEventListener('click', () => {
         .then(res => res.json())
         .then(json => {
             console.log(json)
-            valut.innerHTML = Object.values(json[0].currencies).map(el => el.name)           
+            valut.innerHTML = Object.values(json[0].currencies).map(el => el.name)
             capital.innerHTML = json[0].capital
             symbol.innerHTML = Object.values(json[0].currencies).map(el => el.symbol)
             flag.src = json[0].flags.png
@@ -67,5 +69,26 @@ submit.addEventListener('click', () => {
             region.innerHTML = json[0].region
             maps.href = json[0].maps.googleMaps
             maps.innerHTML = json[0].maps.googleMaps
+
+
+            let values1 = json[0].capital[0]
+            fetch(`http://api.weatherapi.com/v1/current.json?key=82b0db98f6ea4728b0860820230811&q=${values1}`)
+                .then(data => data.json())
+                .then(json => {
+                    console.log(json)
+                    temp.innerHTML = json.current.temp_c
+                    city.innerHTML = json.location.region
+                    text1.innerHTML = json.current.condition.text
+                })
         })
+})
+
+
+
+
+searchInput.addEventListener('keypress', () => {
+    if (event.key === 'Enter') {
+        event.preventDefault()
+        submit.click()
+    }
 })
